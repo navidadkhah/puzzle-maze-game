23c9895992d2a04c3668c9129b6139dc453933c0
navid dadkhah <nnd1380@gmail.com>
2024-11-19 18:16:22 +0330

    fixed impossible game

--- ImpossibleMaze4.py
+++ ImpossibleMaze4.py
@@ -6,36 +6,46 @@ def impossible_maze():
     print("hereee")
     # Define mini-maze settings
     mini_maze = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1],
-        [1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-        [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
     mini_cell_size = 20
+    mini_grid_size = 14  # Size of the maze (9x9)
+
+    mini_screen_size = mini_grid_size * mini_cell_size
+    new_screen = pygame.display.set_mode((mini_screen_size, mini_screen_size))
+
+    arrow_image = pygame.image.load('Images/Arrow.jpg')
+    arrow_image = pygame.transform.scale(arrow_image, (mini_cell_size, mini_cell_size))  # Scale it to fit the cell
+
+    pygame.display.set_caption("Mini Maze Game")
     mini_player_pos = [5, 0]
 
-    # Create a new window for the mini maze
-    new_screen = pygame.display.set_mode((300, 300))
-    pygame.display.set_caption("Mini Maze Game")
 
     def draw_mini_maze():
-        for row in range(13):
-            for col in range(13):
+        for row in range(mini_grid_size):
+            for col in range(mini_grid_size + 1):
                 color = BLACK if mini_maze[row][col] == 1 else WHITE
                 pygame.draw.rect(new_screen, color,
                                  (col * mini_cell_size, row * mini_cell_size, mini_cell_size, mini_cell_size))
-        pygame.draw.rect(new_screen, RED, (
-            mini_player_pos[1] * mini_cell_size, mini_player_pos[0] * mini_cell_size, mini_cell_size, mini_cell_size))
+        center_x = mini_player_pos[1] * mini_cell_size + mini_cell_size // 2
+        center_y = mini_player_pos[0] * mini_cell_size + mini_cell_size // 2
+        pygame.draw.circle(new_screen, RED, (center_x, center_y), mini_cell_size // 2)
+        new_screen.blit(arrow_image, (0 * mini_cell_size, 5 * mini_cell_size))
+        new_screen.blit(arrow_image, (13 * mini_cell_size, 5 * mini_cell_size))
+
 
     draw_mini_maze()
     pygame.display.flip()
@@ -63,7 +73,7 @@ def impossible_maze():
                 draw_mini_maze()
                 pygame.display.flip()
 
-        if mini_player_pos == [5, 12]:
+        if mini_player_pos == [5, 13]:
             waiting_for_key = False
             print("You solved the puzzle!!!")
             score = 60
